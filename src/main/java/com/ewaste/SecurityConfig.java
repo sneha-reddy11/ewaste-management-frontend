e97110d9cc32b2a96ae149b421392245e335f13f
KAVIPRIYA S <kavi472111@gmail.com>
2026-02-21 17:40:04 +0530

    my latest work

--- src/main/java/com/ewaste/SecurityConfig.java
+++ src/main/java/com/ewaste/SecurityConfig.java
@@ -1,21 +1,22 @@
 package com.ewaste;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import com.ewaste.security.JwtAuthFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.ewaste.security.JwtAuthFilter;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -33,16 +34,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",
-                                "/index.html",
-                                "/*.html",
-                                "/css/**",
-                                "/js/**",
-                                "/auth/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
+        .requestMatchers(
+                "/",
+                "/index.html",
+                "/*.html",
+                "/css/**",
+                "/js/**",
+                "/auth/**",
+                "/error"
+        ).permitAll()
+        .anyRequest().authenticated()
+)
+
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
